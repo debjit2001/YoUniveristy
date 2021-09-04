e3c7ec9b22f7dc987c063b74d48699826ff29337
Pronay Guha <pronayguha13@gmail.com>
2021-09-04 23:52:51 +0530

    Image upload fix with multer and cloudinary

---
+++ backend/utils/multer.js
@@ -0,0 +1,34 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  //reject
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/svg+xml"
+  ) {
+    //accept
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid File type"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
