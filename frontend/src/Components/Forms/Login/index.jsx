69daf1b29afb52b211ef3f3805d5f78539820a5d
Pronay Guha <pronayguha13@gmail.com>
2021-11-09 23:32:08 +0530

    form fixation with stepper

--- frontend/src/Components/Forms/Login/index.jsx
+++ frontend/src/Components/Forms/Login/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MailOutline } from "react-ionicons";
+
 import styles from "./style.module.css";
 
-import { Link } from "react-router-dom";
 import {
   validateEmail,
   validatePassword,
@@ -16,6 +18,7 @@ const Login = () => {
   });
   let name, value;
   var error = false;
+
   const handleInputs = (e) => {
     name = e.target.name;
     value = e.target.value;
@@ -39,8 +42,8 @@ const Login = () => {
       setErrorMessage = isValid[1];
     }
     console.log(isValid);
-    warningIcon.style.display = "none";
-    correctIcon.style.display = "none";
+    // warningIcon.style.display = "none";
+    // correctIcon.style.display = "none";
 
     if (value.length === 0) {
       nameField.style.borderBottom = "2px solid black";
@@ -58,143 +61,93 @@ const Login = () => {
 
   return (
     <>
-      <div className={`${styles.container_login}`}>
-        <div className={`${styles.form_outer}`}>
-          <div className={`${styles.form_inner}`}>
-            <p className={`${styles.reg}`}>Login</p>
-            <div className={`${styles.formpng}`}>
-              <div className={`${styles.form_inner_inner}`}>
-                <form action="login" method="POST">
-                  <div className={`${styles.form_row}`}>
-                    <i
-                      className={`fa fa-user ${styles.fa_user}`}
-                      aria-hidden="true"
-                    ></i>
-                    <div
-                      className={`${styles.form_name}`}
-                      id="registrationNumber"
-                    >
-                      <div className={`${styles.col}`}>
-                        <input
-                          type="email"
-                          className={`${styles.form_control}`}
-                          value={teacherLogin.registrationNumber}
-                          onChange={handleInputs}
-                          placeholder="Enter your Registration Number"
-                          name="registrationNumber"
-                        />
-                        <i
-                          class="fas fa-exclamation-circle"
-                          style={{ color: "#f60000" }}
-                        ></i>
-                        <i
-                          class="fas fa-check-circle"
-                          style={{ color: "#005f00" }}
-                        ></i>
-                      </div>
-                      <div className="errorMessage"></div>
-                    </div>
-                  </div>
-                  <div className={`${styles.form_row}`}>
-                    <i
-                      className={`fa fa-user ${styles.fa_user}`}
-                      aria-hidden="true"
-                    ></i>
-                    <div className={`${styles.form_name}`} id="email">
-                      <div className={`${styles.col}`}>
-                        <input
-                          type="email"
-                          className={`${styles.form_control}`}
-                          value={teacherLogin.email}
-                          onChange={handleInputs}
-                          placeholder="Enter your email"
-                          name="email"
-                        />
-                        <i
-                          class="fas fa-exclamation-circle"
-                          style={{ color: "#f60000" }}
-                        ></i>
-                        <i
-                          class="fas fa-check-circle"
-                          style={{ color: "#005f00" }}
-                        ></i>
-                      </div>
-                      <div className="errorMessage"></div>
-                    </div>
-                  </div>
-                  <div className={`${styles.form_row}`}>
-                    <i
-                      className={`fa fa-lock ${styles.fa_lock}`}
-                      aria-hidden="true"
-                    ></i>
-                    <div className={`${styles.form_name}`} id="password">
-                      <div className={`${styles.col}`}>
-                        <input
-                          type="password"
-                          className={`${styles.form_control}`}
-                          value={teacherLogin.password}
-                          onChange={handleInputs}
-                          placeholder="Enter your password"
-                          name="password"
-                        />
-                        <i
-                          class="fas fa-exclamation-circle"
-                          style={{ color: "#f60000" }}
-                        ></i>
-                        <i
-                          class="fas fa-check-circle"
-                          style={{ color: "#005f00" }}
-                        ></i>
-                      </div>
-                      <div className="errorMessage"></div>
-                    </div>
-                  </div>
-                  <div className={`${styles.forget_password}`}>
-                    <Link to="/">Forget Password ?</Link>
-                  </div>
-                  <div className={`${styles.form_check}`}>
-                    <input
-                      type="checkbox"
-                      className={`${styles.form_check_input}`}
-                      id="exampleCheck1"
-                    />
-                    <label
-                      className={`${styles.form_check_label}`}
-                      htmlFor="exampleCheck1"
-                    >
-                      Remember Me
-                    </label>
-                  </div>
-                  <div className={`${styles.btner}`}>
-                    <button
-                      type="submit"
-                      className={`btn btn-danger ${styles.login}`}
-                    >
-                      LogIn
-                    </button>
-                    <div className={`${styles.no_account}`}>
-                      Don't have an account ?
-                      <Link
-                        style={{
-                          marginLeft: "14px",
-                          cursor: "pointer",
-                          color: "#1c3958",
-                          fontWeight: "bold",
-                        }}
-                        to="/register/teacher"
-                      >
-                        Register
-                      </Link>
-                    </div>
-                  </div>
-                </form>
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <p className={styles.header}>Login</p>
+          <div className={styles.formBody}>
+            <div className={styles.form_row}>
+              <i
+                className={`fa fa-user ${styles.fa_user}`}
+                aria-hidden="true"
+              ></i>
+              <div className={styles.col}>
+                <input
+                  type="email"
+                  className={styles.form_control}
+                  value={teacherLogin.registrationNumber}
+                  onChange={handleInputs}
+                  placeholder="Enter your Registration Number"
+                  name="registrationNumber"
+                />
+                <i
+                  className="fas fa-exclamation-circle"
+                  style={{ color: "#f60000" }}
+                ></i>
+                <i
+                  className="fas fa-check-circle"
+                  style={{ color: "#005f00" }}
+                ></i>
               </div>
-              <div className={`${styles.form_inner_inner}`}>
-                {/* <img
-                  className={`${styles.login_png}`}
-                  src={loginSvg}
-                  alt="login_photo"
-                /> */}
+            </div>
+            <div className={styles.form_row}>
+              <div className={styles.form_name} id="email">
+                <i className={`fa fa-envelope ${styles.fa_user}`}></i>
+                <input
+                  type="email"
+                  className={styles.form_control}
+                  value={teacherLogin.email}
+                  onChange={handleInputs}
+                  placeholder="Enter your email"
+                  name="email"
+                />
+              </div>
+            </div>
+            <div className={`${styles.form_row} ${styles.passwordInput}`}>
+              <i
+                className={`fa fa-lock ${styles.fa_lock}`}
+                aria-hidden="true"
+              ></i>
+              <div className={styles.form_name} id="password">
+                <div className={styles.col}>
+                  <input
+                    type="password"
+                    className={styles.form_control}
+                    value={teacherLogin.password}
+                    onChange={handleInputs}
+                    placeholder="Enter your password"
+                    name="password"
+                  />
+                  <i
+                    className="fas fa-exclamation-circle"
+                    style={{ color: "#f60000" }}
+                  ></i>
+                  <i
+                    className="fas fa-check-circle"
+                    style={{ color: "#005f00" }}
+                  ></i>
+                </div>
+              </div>
+            </div>
+            <div className={styles.btner}>
+              <button
+                type="submit"
+                className={`btn btn-danger ${styles.loginButton}`}
+              >
+                Log In
+              </button>
+              <div className={styles.no_account}>
+                Don't have an account ?
+                <Link
+                  style={{
+                    marginLeft: "14px",
+                    cursor: "pointer",
+                    color: "#1c3958",
+                    fontWeight: "bold",
+                  }}
+                  to="/register/teacher"
+                >
+                  Register
+                </Link>
               </div>
             </div>
           </div>
