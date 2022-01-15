ccbb80d72486a01e80ba62b9b709929f4dbc812a
Pronay Guha <pronayguha13@gmail.com>
2022-01-15 22:02:46 +0530

    refactoring attendanceIndexPage

--- frontend/src/Components/AttendancePage/IndexPage/index.jsx
+++ frontend/src/Components/AttendancePage/IndexPage/index.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 import { LoginContext } from "../../../global/LoginContext";
+import AttendanceCharacterCard from "../../AttendanceCharacterCard";
+import LinkButton from "../../LinkButton";
 
 const Attendance = () => {
   const { isTeacherAuth, setIsTeacherAuth, teacherID, pwd } =
@@ -16,27 +18,28 @@ const Attendance = () => {
   return (
     <div className={`${styles.outer_container}`}>
       <div className={`${styles.login_register}`}>
-        <Link className={`${styles.button}`} to="/signin">Login</Link>
-        <Link className={`${styles.button}`} to="/signup">Register</Link>
+        {/* <Link className={`${styles.button}`} to="/signin">
+          Login
+        </Link>
+        <Link className={`${styles.button}`} to="/signup">
+          Register
+        </Link> */}
+        <LinkButton targetUrl="signin" buttonText="Login" />
+        <LinkButton targetUrl="signup" buttonText="Register" />
       </div>
-    <div className={styles.container}>
-      <h2 className={styles.heading}>WHO ARE YOU?</h2>
-      <Link to="/attendanceStudentSemester">
-        <button type="button" className={styles.btn}>
-          I'm a student
-        </button>
-        <div className="container-fluid"></div>
-      </Link>
-      <br />
-      <br />
-      <Link to={isTeacherAuth ? "/teacherWelcome" : "/login/teacher"}>
-        <button type="button" className={styles.btn}>
-          I'm a teacher
-        </button>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>WHO ARE YOU?</h2>
+        <AttendanceCharacterCard
+          targetUrl="attendanceStudentSemester"
+          buttonText="I'm a student"
+        />
         <br />
         <br />
-      </Link>
-    </div>
+        <AttendanceCharacterCard
+          targetUrl={isTeacherAuth ? "/teacherWelcome" : "/login/teacher"}
+          buttonText="I'm a teacher"
+        />
+      </div>
     </div>
   );
 };
