0730f8a32f2f60b8ca8053978aecf54dedc96f2d
Pronay Guha <pronayguha13@gmail.com>
2022-01-19 21:49:16 +0530

    Added props & loader implementation for Image Wrapper

--- frontend/src/Components/Image-Wrapper/index.jsx
+++ frontend/src/Components/Image-Wrapper/index.jsx
@@ -1,8 +1,22 @@
 //Third party import
 import React from "react";
+//local import
+import Loading from "Components/Loading";
 
-const ImageWrapper = ({ imgSrc, imgAlt }) => {
-  return <img src={imgSrc} alt={imgAlt} />;
+const ImageWrapper = ({
+  imgSrc,
+  imgAlt,
+  isLoading,
+  loader,
+  className,
+  onLoadHandler,
+  onClickHandler,
+}) => {
+  return isLoading ? (
+    loader || <Loading loading={isLoading} />
+  ) : (
+    <img src={imgSrc} alt={imgAlt} />
+  );
 };
 
 export default ImageWrapper;
