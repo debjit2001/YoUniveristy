f229eafaee1e8b9bd035b0923f2dbd1dfb3acd62
Pronay Guha <pronayguha13@gmail.com>
2022-01-22 16:59:37 +0530

    destructured props for stream dropdown

--- frontend/src/Components/AttendanceStudentSemester/index.jsx
+++ frontend/src/Components/AttendanceStudentSemester/index.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./style.module.css";
-const AttendanceStudentSemester = (buttonStyle,clickHandler,buttonText) => {
-    const _onClickHandler=e=>clickHandler(e)
-    return (
-        <div>
-            {console.log(buttonStyle)}
-            <button className= {`dropdown-item ${styles.buttonStyle}`} type="button" onClick={_onClickHandler}>
-                {buttonText}
-            </button>
-        </div>
-    )
-}
+const AttendanceStudentSemester = ({
+  buttonStyle,
+  clickHandler,
+  buttonText,
+}) => {
+  const _onClickHandler = (e) => {
+    clickHandler(e);
+  };
+  return (
+    <div>
+      {console.log(buttonStyle)}
+      <button
+        className={`dropdown-item ${styles.buttonStyle}`}
+        type="button"
+        onClick={_onClickHandler}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
-export default AttendanceStudentSemester
+export default AttendanceStudentSemester;
