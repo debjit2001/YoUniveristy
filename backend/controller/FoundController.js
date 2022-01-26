f12d6c4e1207e88de013e45620d2d3d5f14ffba9
Pronay Guha <pronayguha13@gmail.com>
2022-01-26 20:49:05 +0530

    mail service added and tested for found section

--- backend/controller/FoundController.js
+++ backend/controller/FoundController.js
@@ -1,6 +1,31 @@
-const Found = require("../models/Found");
 const cloudinary = require("../utils/cloudinary");
+const nodemailer = require("nodemailer");
+const Found = require("../models/Found");
 const helperMethods = require("../utils/FoundHelper");
+const mailerConfig = require("../config/mailerConfig");
+
+//function to send email whenever a item found is registered
+const mailHandler = async (email, itemName, name) => {
+  let mailOptions = {
+    from: "Youniversity.official.2022@gmail.com",
+    to: email,
+    subject: `Found Item registered`,
+    html: `
+    <p>Hey <b>${name}</b>,</p>
+    <p>Your entry for item <b>${itemName}</b> has been registered successfully.</p>
+    <p>Thanks and Regards</p>
+    <p><b>Team YOUniversity</b></p>
+    `,
+  };
+
+  mailerConfig.transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
 
 exports.create_found_entry = async (req, res) => {
   const { name, email, itemName, foundDate, foundItemDetails } = req.body;
@@ -31,6 +56,7 @@ exports.create_found_entry = async (req, res) => {
       try {
         const newEntry = await newFoundItem.save();
         res.status(200).json({ newFoundEntry: newEntry });
+        mailHandler(email, itemName, name);
       } catch (saveError) {
         res.status(500).json({ newFoundEntry: null });
       }
