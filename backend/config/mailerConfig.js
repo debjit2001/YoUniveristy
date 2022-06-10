8e2f33e67d3a7acef6ed791831997c20c59b8a68
debjit2001 <65007559+debjit2001@users.noreply.github.com>
2022-06-11 01:46:20 +0530

    MailerConfig Commented

--- backend/config/mailerConfig.js
+++ backend/config/mailerConfig.js
@@ -1,17 +1,17 @@
-const nodemailer = require("nodemailer");
-const smtpPool = require('nodemailer-smtp-pool');
+// const nodemailer = require("nodemailer");
+// const smtpPool = require('nodemailer-smtp-pool');
 
 
-exports.transporter = nodemailer.createTransport(smtpPool({
-  // host: "smtp.gmail.com",
-  // port: 587,
-  // secure: false,
-  // requireTLS: true,
-  service: 'gmail',
-  auth: {
-    user: process.env.MAILER_ID,
-    pass: process.env.MAILER_PASSWORD,
-  },
-  maxConnections: 5,
-  maxMessages: 100
-}));
+// exports.transporter = nodemailer.createTransport(smtpPool({
+//   // host: "smtp.gmail.com",
+//   // port: 587,
+//   // secure: false,
+//   // requireTLS: true,
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.MAILER_ID,
+//     pass: process.env.MAILER_PASSWORD,
+//   },
+//   maxConnections: 5,
+//   maxMessages: 100
+// }));
